2bb1e17b20603421e2c21f994d880688aac4b023
startupmarketing <startumarketing.si@gmail.com>
2018-06-29 12:40:10 +0000

    routes bugs

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const currencyExchangeRoutes = require('./api/routes/currency_exchange');
 
-app.use('/currency_exchange', currencyExchangeRoutes);
+app.use('/', currencyExchangeRoutes);
 
 module.exports = app;
