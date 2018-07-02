c9b8ddbe78bfd8ec5f868344a0f31237026ce4cc
Rok <rkeglevic@gmail.com>
2018-07-03 00:04:18 +0200

    api za pretvorbo določene vsote denarja narjen

--- api/routes/currency_exchange.js
+++ api/routes/currency_exchange.js
@@ -2,11 +2,14 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
+
+//Function for obtaining exchange rate from internet
 async function getExchange(currency_exchange_joined){
 	var data = false;
 	await axios.get('https://free.currencyconverterapi.com/api/v5/convert?q=' + currency_exchange_joined + '&compact=ultra')
 	  .then(response => {
 	  	data = response.data;
+	  	console.log("Exchange rate obtained!")
 	    console.log(response.data);
 	  })
 	  .catch(error => {
@@ -15,6 +18,8 @@ async function getExchange(currency_exchange_joined){
 	return data;
 }
 
+
+//Testing req/res
 router.get('', (req, res, next) => {
 	res.status(200).json({
 		message: 'Get request handled!'
@@ -27,16 +32,47 @@ router.post('', (req, res, next) => {
 	});
 });
 
-router.get('/nlbvita/currency_exchange/:currency_exchange_query', async (req, res, next) => {
-
+//API for currency exchange rate
+router.get('/:currency_exchange_query', async (req, res, next) => {
+	//First we get query for exchange and exchange rate from other api
 	const currency_exchange_joined = req.params.currency_exchange_query;
 	var exchangeRate = await getExchange(currency_exchange_joined);
+
+	//We check if exchange rate was obtained and respond with data
 	if(Object.keys(exchangeRate).length !== 0){
 		res.status(200).json({
 			"messages": [
 				{"text": "Welcome to the NLB Vita Exchange API"},
 				{"text": "Exchange rate you requested is: "},
 				{"text": Object.values(exchangeRate).toString() }
+			]	
+		});
+	}else{
+		res.status(200).json({
+			message: 'Your input of short Currency names was inccorect!',
+			currency_exchange_joined: currency_exchange_joined
+		});
+	};
+});
+
+router.post('/:currency_exchange_query', async (req, res, next) => {
+	//First we get query for exchange and exchange rate from other api
+	const currency_exchange_joined = req.params.currency_exchange_query;
+	var jsonString = '';
+	var exchangeRate = await getExchange(currency_exchange_joined);
+
+
+	//We extract amount of money that will be changed from post data
+	var post_data = req.body;
+
+	
+	//We check if exchange rate was obtained and respond with data
+	if(Object.keys(exchangeRate).length !== 0){
+		res.status(200).json({
+			"messages": [
+				{"text": "Welcome to the NLB Vita Exchange API"},
+				{"text": "Info about exchange you requested is: "},
+				{"text": (Object.values(exchangeRate) * post_data.amount).toString() }
 			]	
 		});
 	}else{
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const currencyExchangeRoutes = require('./api/routes/currency_exchange');
+const webviewsRoutes = require('./api/routes/webviews');
+const bodyParser = require('body-parser')
 
-app.use('/', currencyExchangeRoutes);
+
+app.use(bodyParser.json())
+app.use('/nlbvita/currency_exchange', currencyExchangeRoutes);
+app.use('/nlbvita/webviews', webviewsRoutes);
 
 module.exports = app;
